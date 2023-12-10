aa61c0c64546eb8360f81a02f183314db581741a
Code Hangton <code.hangton@gmail.com>
2023-12-10 12:59:42 +0800

    new gpt modal

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -49,6 +49,11 @@ export async function POST(request: NextRequest) {
       },
     ],
     model: "gpt-3.5-turbo",
+    temperature: 1.2,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0.5,
+    presence_penalty: 0.5,
   });
   const content = chatCompletion.choices[0].message.content || "";
 
